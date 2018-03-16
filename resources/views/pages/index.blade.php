aa26d8933479b915c4827712b02be1caaa0140bf
Nicholas Konstantino <nkonstantino91@gmail.com>
2018-03-15 22:29:18 -0700

    init

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>Welcome to Laravel!</h1>
+        <p>This is the laravel app I'm making from Youtube.</p>
+        <p><a href="/login" role="button" class="btn btn-primary btn-lg">Login</a>  <a href="/register" role="button" class="btn btn-success btn-lg">Sign Up</a></p>
+    </div>
+@endsection
